d9c6d92d808fe8d3dff86f9d53d94be96f6721da
aPhoneixCat <julianzhu@astri.org>
2024-05-27 01:34:49 -0700

    feat: add placeholder controller for wifi information

--- src/api/controllers/wifi.controller.ts
+++ src/api/controllers/wifi.controller.ts
@@ -1,6 +1,5 @@
 import { Get, Post, Body, Path, Route, Tags } from "tsoa";
 import {  } from "../../domain/dtos/acs";
-import { QRCodeType } from "../../domain/entities/cardholder";
 
 // TODO - 1B
 @Route("wifi")
